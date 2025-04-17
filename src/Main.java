ba529a49bb615f607fb77d73246a3494e5473761
24116939 <24116939@student.hhs.nl>
2025-04-17 09:09:02 +0000

    class gemaakt + getters en setters van de class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.scanner;
+
+class Gebruiker {
+
+    private String naam;
+    private int id;
+
+    public Gebruiker (String naam, int id) {
+        this.naam = naam;
+        this.id = id;
+    }
+
+    public void setNaam (String naam) {
+        this.naam = naam;
+    }
+    public String getNaam () {
+        return naam;
+    }
+    public void setId (int id) {
+        this.id = id;
+    }
+    public int getId () {
+        return id;
+    }
+
+}
+
+class chatroom {
+    
+}
+
+
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+
+        Gebruiker gebruiker = new Gebruiker (Stan, 984);
+        System.out.println(gebruiker)
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
